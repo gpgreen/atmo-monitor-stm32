a7cb33bead3ea2a6048352924ad966251ddfdef7
Greg Green <ggreen@Gregs-MacBook-Air.local>
2023-11-29 00:56:34 -0800

    Using average for pms7003 data
    Changed pms-7003 dependency to my github

--- src/pms7003_device.rs
+++ src/pms7003_device.rs
@@ -1,12 +1,11 @@
 //! Reading the Plantower PMS7003 sensor
 
-use defmt::{debug, info, Format};
-use embassy_time::{Duration, Timer};
-//use embedded_io_async::{Read, Write};
 use crate::{parameter::Parameters, DisplayInfo};
+use defmt::{debug, info, Format};
 use embassy_stm32::{peripherals, usart};
 use embassy_sync::blocking_mutex::raw::NoopRawMutex;
 use embassy_sync::channel::Sender;
+use embassy_time::{Duration, Timer};
 use pms_7003::{async_interface::Pms7003SensorAsync, Error};
 
 /// Data from the sensor
@@ -32,6 +31,32 @@ impl PmSensorData {
             pm10_atm: frame.pm10_atm,
         }
     }
+
+    /// calculate moving average from array of PmSensorData
+    pub fn average(data: &[PmSensorData]) -> Self {
+        let mut pm1_0_avg: u32 = 0;
+        let mut pm2_5_avg: u32 = 0;
+        let mut pm10_avg: u32 = 0;
+        let mut pm1_0_atm_avg: u32 = 0;
+        let mut pm2_5_atm_avg: u32 = 0;
+        let mut pm10_atm_avg: u32 = 0;
+        for d in data {
+            pm1_0_avg += d.pm1_0 as u32;
+            pm2_5_avg += d.pm2_5 as u32;
+            pm10_avg += d.pm10 as u32;
+            pm1_0_atm_avg += d.pm1_0_atm as u32;
+            pm2_5_atm_avg += d.pm2_5_atm as u32;
+            pm10_atm_avg += d.pm10_atm as u32;
+        }
+        Self {
+            pm1_0: (pm1_0_avg / data.len() as u32) as u16,
+            pm2_5: (pm2_5_avg / data.len() as u32) as u16,
+            pm10: (pm10_avg / data.len() as u32) as u16,
+            pm1_0_atm: (pm1_0_atm_avg / data.len() as u32) as u16,
+            pm2_5_atm: (pm2_5_atm_avg / data.len() as u32) as u16,
+            pm10_atm: (pm10_atm_avg / data.len() as u32) as u16,
+        }
+    }
 }
 
 ///task to read pm2.5 sensor data
@@ -42,6 +67,8 @@ pub async fn pm25_controller(
     _params: Parameters,
 ) {
     info!("starting pm2.5 loop");
+    let mut data = [PmSensorData::default(); 5];
+    let mut offset = 0;
     loop {
         //info!("sending passive cmd to sensor");
         //dev.passive().await.unwrap();
@@ -50,43 +77,15 @@ pub async fn pm25_controller(
             "PM1_0: {} PM2_5: {} PM10: {} PM1_0_atm: {} PM2_5_atm {}, PM10_atm {}",
             frame.pm1_0, frame.pm2_5, frame.pm10, frame.pm1_0_atm, frame.pm2_5_atm, frame.pm10_atm,
         );
-        sender
-            .send(DisplayInfo::Pms7003Data(PmSensorData::copy_from_frame(
-                &frame,
-            )))
-            .await;
+        if offset < data.len() {
+            data[offset] = PmSensorData::copy_from_frame(&frame);
+            offset += 1;
+        }
+        if offset == data.len() {
+            offset = 0;
+            let avg = PmSensorData::average(&data);
+            sender.send(DisplayInfo::Pms7003Data(avg)).await;
+        }
         Timer::after(Duration::from_millis(250)).await;
     }
 }
-
-// /// Structure for PMS7003 device attached to Serial
-// pub struct PMS7003Device<Serial> {
-//     dev: Pms7003SensorAsync<Serial>,
-// }
-
-// impl<Serial> PMS7003Device<Serial>
-// where
-//     Serial: embedded_io_async::Read + embedded_io_async::Write + embedded_io_async::ErrorType,
-// {
-//     pub fn new(usart: Serial) -> Self {
-//         Self {
-//             dev: Pms7003SensorAsync::new(usart),
-//         }
-//     }
-
-//     pub fn sleep(&mut self) -> Result<(), Error> {
-//         self.dev.sleep().after?
-//     }
-
-//     pub fn wake(&mut self) -> Result<(), Error> {
-//         self.dev.wake().after?
-//     }
-
-//     pub async fn passive_read(&mut self) -> Result<(), Error> {
-//         self.dev.passive().after?;
-//         Timer::after(Duration::from_secs(1)).await;
-//         let frame = self.dev.read()?;
-//         debug!("frame received {:?}", frame);
-//         Ok(())
-//     }
-// }
